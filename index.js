db566e2f3b82b5d30222be1c33e152f659099a8e
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2021-06-25 14:26:46 -0300

    Fixed glitch on hover search buttons

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-// function elFetch() {
-//   fetch("https://api.chucknorris.io/jokes/random")
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .then((res) => {
-//       console.log(res.value);
-//     });
-// }
-
-function elFetch(palabra) {
+function fetchBySearch(palabra) {
   fetch(
     "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/search?query=" +
       palabra,
@@ -34,29 +24,58 @@ function elFetch(palabra) {
     });
 }
 
-function handleSubmit(e) {
-  e.preventDefault();
-  clearJokes();
-  const palabraClave = this.buscar.value;
-  elFetch(palabraClave);
+function fetchRandom() {
+  fetch(
+    "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random",
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-rapidapi-key": "968bdcd1f8msh8dce65d1dab0d40p1b4845jsn3688faf1a955",
+        "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+      },
+    }
+  )
+    .then((response) => {
+      // console.log(response);
+      return response.json();
+    })
+    .then((response) => {
+      console.log(response);
+      cloningTemplate(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
-function cloningTemplate(collection) {
+function handleSubmit(e) {
+  e.preventDefault();
+  // console.log(e.submitter.classList.value);
+  clearJokes();
+  const palabraClave = this.buscar.value;
+  if (e.submitter.classList.value == "search-button") {
+    fetchBySearch(palabraClave);
+    // console.log("search button");
+  } else if (e.submitter.classList.value == "lucky-button") {
+    fetchRandom();
+    // console.log("lucky button");
+  }
+}
+
+function cloningTemplate(obj) {
   const resultsContainer = document.querySelector(".results-container");
 
   const template = document.querySelector("#joke-card");
 
-  for (const joke of collection.result) {
-    var clone = template.content.cloneNode(true);
-    // console.log(joke.url);
-    const resultLink = clone.querySelector(".result-link");
-    resultLink.href = joke.url;
-    resultLink.textContent = joke.url;
-    const resultInformation = clone.querySelector(".result-information");
-    resultInformation.textContent = joke.value;
+  var clone = template.content.cloneNode(true);
+  const resultLink = clone.querySelector(".result-link");
+  resultLink.href = obj.url;
+  resultLink.textContent = obj.url;
+  const resultInformation = clone.querySelector(".result-information");
+  resultInformation.textContent = obj.value;
 
-    resultsContainer.appendChild(clone);
-  }
+  resultsContainer.appendChild(clone);
 }
 
 function clearJokes() {
